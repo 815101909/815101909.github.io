9f349629cc64caed1d89c7524e6be9a3fda2e28a
815101909 <815101909@qq.com>
2023-12-09 01:55:38 +0800

    Site updated: 2023-12-09 01:55:37

--- js/fomal.js
+++ js/fomal.js
@@ -2785,11 +2785,11 @@ var now = new Date();
 function createtime() {
   // 当前时间
   now.setTime(now.getTime() + 1000);
-  var start = new Date("04/12/2023 19:18:00"); // 旅行者1号开始计算的时间
+  var start = new Date("12/04/2023 19:18:00"); // 旅行者1号开始计算的时间
   var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
   var unit = (dis / 149600000).toFixed(6);  // 天文单位
   // 网站诞生时间
-  var grt = new Date("04/12/2023 19:18:00");
+  var grt = new Date("12/04/2023 19:18:00");
   var days = (now - grt) / 1e3 / 60 / 60 / 24,
     dnum = Math.floor(days),
     hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
